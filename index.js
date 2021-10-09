07f8b5df9050905fab2c5a558cbb219589f75a6c
Kostya <kosgoh.214@gmail.com>
2021-10-09 14:23:39 +0300

    first practise

---
+++ index.js
@@ -0,0 +1,22 @@
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {}, 
+    genres: {},
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
